aa7012c8b2674579f3cb7a50a11b280d50fe8cd4
mikhail-cct <tayemnyj+cct@gmail.com>
2021-12-10 16:07:58 +0000

    wk12 index.js

--- wk12/index.js
+++ wk12/index.js
@@ -6,3 +6,10 @@ const   router = express(),
         server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname,'views')));
+
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    let addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
+})
